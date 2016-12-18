ebbbfed99d5b953f0352b6b30f32c665c1e90f79
Ayo Isaiah <sudo@ayoisaiah.com>
2016-12-18 22:55:06 +0100

    Minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,51 @@
-var gulp = require("gulp");
-var shell = require("gulp-shell");
-var browserSync = require("browser-sync").create();
+const gulp = require('gulp');
 
-gulp.task("build", shell.task(["jekyll serve --watch"]));
-gulp.task("serve", function () {
-	browserSync.init({server: {baseDir: "_site/"}});
-	gulp.watch("_site/**/*.*").on("change", browserSync.reload);
+// const concat = require('gulp-concat');
+// const rename = require('gulp-rename');
+// const uglify = require('gulp-uglify');
+
+// const jsfiles = 'assets/vendor/mdl/scripts/**/*.js';
+
+const child = require('child_process');
+const gutil = require('gulp-util');
+
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site';
+
+// gulp.task('scripts', function () {
+//   return gulp.src(jsfiles)
+//   .pipe(concat('index.js'))
+//   .pipe(gulp.dest('assets/js'))
+//   .pipe(rename('index.min.js'))
+//   .pipe(uglify())
+//   .pipe(gulp.dest('assets/js'))
+// });
+
+gulp.task('jekyll', function () {
+  const jekyll = child.spawn('jekyll', ['build', '--watch', 'incremental', '--drafts']);
+
+  const jekyllLogger = function (buffer) {
+    buffer.toString()
+    .split(/\n/)
+    .forEach(function (message) {
+      gutil.log('Jekyll: ' + message);
+    });
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
-gulp.task("default", ["build", "serve"]);
+gulp.task('serve', function () {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 3000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+  // gulp.watch(jsfiles, ['scripts']);
+});
+
+gulp.task('default', ['jekyll', 'serve']);
